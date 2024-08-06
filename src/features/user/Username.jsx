9e81d8bae66db30c46f62a7a14d94ee1f5549c3a
Moses-sunday <mrmusco123@yahoo.com.com>
2024-08-06 16:39:34 +0100

    username

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,7 +1,15 @@
+import { useSelector } from "react-redux";
+
 function Username() {
-  return <div className="px-12 mt-3">
-    <h1 className=" sm:block sm:uppercase sm:text-red-500 sm:mx-4 hidden">moses</h1>;
-  </div>
+  const username = useSelector((state) => state.user.username);
+  return (
+    <div className="px-12 mt-3">
+      <h1 className=" sm:block sm:uppercase sm:text-red-500 sm:mx-4 hidden">
+        {username}
+      </h1>
+      ;
+    </div>
+  );
 }
 
 export default Username;
